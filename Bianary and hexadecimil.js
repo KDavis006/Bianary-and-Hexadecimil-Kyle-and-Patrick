03fb7315c5a8293ed09c0377adf8b6a9b41b3b69
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2022-09-02 22:36:50 -0700

    new code for bianary

--- Bianary and hexadecimil.js
+++ Bianary and hexadecimil.js
@@ -1,25 +1,38 @@
+// while(num.length < 9){
+//  num = '0' + num
+//  if(num.length == 8){
+//   return num
+//  }
+// }
+//}
+
 // This first let statement is asking what the input you want to insert
 let number = Number(prompt("What is the decimal form?"))
+
 // This let statement converts the number into bianary
 let result = number.toString(2)
+
 // This sets it to a string
 result = String(result)
-// This makes it so where the bianary is 8 digits
-function convert(numb){
- let num = numb
- // Puts it in a while loop until it has 8 digits
- while(num.length < 9){
-  num = '0' + num
-  if(num.length == 8){
-   return num
-  }
- }
-}
+
+// Gives you the length
+let lenOfNum = result.length
+//Makes the length into your bytes you need
+let bytes = lenOfNum / 8
+//Making the bytes into a whole number and determining number of bits required
+let bits = Math.ceil(bytes) * 8
+//Pads the front to the next byte
+let resultFinal = result.padStart(bits,"0")
+
 // This outputs your answer for the bianary
-alert("The Bianary of " + number + " is " + convert(result))
+alert("The Bianary of " + number + " is " + resultFinal)
 
 
 // This converts your input to hexadecimal
 let result2 = number.toString(16)
 // This is the output for the hexadecimal
 alert("The Hexadecimal of " + number + " is " + result2)
+
+
+
+console.log(resultFinal)
